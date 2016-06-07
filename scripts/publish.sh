2a6bd484f7621c94ba23bc72ce04a34d4900bb74
Benjamin Kovach <bkovach5@uga.edu>
2016-06-07 09:16:55 -0400

    Add publish script

---
+++ scripts/publish.sh
@@ -0,0 +1,10 @@
+#! /bin/zsh
+
+site build
+cp -r _site/* ../5outh.github.io
+cd ../5outh.github.io
+echo "Edits include:"
+git status
+git add . -A
+git commit -m "Automated publish"
+git push origin master
