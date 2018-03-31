1acbb455844adfc0afed6b08df2cfc5b819ea221
5outh <bkovach13@gmail.com>
2018-03-31 08:18:47 -0400

    Woops

--- js/site.js
+++ js/site.js
@@ -1,9 +1,9 @@
 $(document).ready(function () {
   $('video').each(function() {
-    this.onclick(function () {
+    this.onclick = function () {
       var video = $('video').get()[0];
       video.currentTime = 0;
       video.play();
-    })
+    }
   })
 })
